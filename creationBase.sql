2fb6e2aea0098f5dfe22927b713e33488896be0b
Giraudux <giraudux@gmail.com>
2015-03-19 09:53:45 +0000

    to lower case + replace id_film by id_salle in achat

--- creationBase.sql
+++ creationBase.sql
@@ -1,62 +1,62 @@
-SET ECHO OFF
+set echo off
 
 prompt Mise en place de la base de données...
 
 -- Suppression des tables (si) présentes dans la base avant création
-DROP TABLE Genre CASCADE CONSTRAINTS PURGE;
-DROP TABLE Restriction CASCADE CONSTRAINTS PURGE;
-DROP TABLE Salle CASCADE CONSTRAINTS PURGE;
-DROP TABLE Tarif CASCADE CONSTRAINTS PURGE;
-DROP TABLE Version CASCADE CONSTRAINTS PURGE;
-DROP TABLE Film CASCADE CONSTRAINTS PURGE;
-DROP TABLE Seance CASCADE CONSTRAINTS PURGE;
-DROP TABLE Achat CASCADE CONSTRAINTS PURGE;
+drop table Genre cascade constraints purge;
+drop table Restriction cascade constraints purge;
+drop table Salle cascade constraints purge;
+drop table Tarif cascade constraints purge;
+drop table Version cascade constraints purge;
+drop table Film cascade constraints purge;
+drop table Seance cascade constraints purge;
+drop table Achat cascade constraints purge;
 
 -- Création de la base de données
-CREATE TABLE Genre
-(id_genre NUMBER(4) PRIMARY KEY NOT NULL,
-lib_genre VARCHAR2(64));
+create table Genre
+(id_genre number(4) primary key not null,
+lib_genre varchar2(64));
 
-CREATE TABLE Restriction
-(id_restriction NUMBER(4) PRIMARY KEY NOT NULL,
-lib_restriction VARCHAR2(64));
+create table Restriction
+(id_restriction number(4) primary key not null,
+lib_restriction varchar2(64));
 
-CREATE TABLE Salle
-(id_salle NUMBER(3) PRIMARY KEY NOT NULL,
-capacite NUMBER(4));
+create table Salle
+(id_salle number(3) primary key not null,
+capacite number(4));
 
-CREATE TABLE Tarif
-(type_tarif NUMBER(4) PRIMARY KEY NOT NULL,
-lib_tarif VARCHAR2(64),
-prix NUMBER(2,2));
+create table Tarif
+(type_tarif number(4) primary key not null,
+lib_tarif varchar2(64),
+prix number(2,2));
 
-CREATE TABLE Version
-(id_version NUMBER(4) PRIMARY KEY NOT NULL,
-lib_version VARCHAR2(64));
+create table Version
+(id_version number(4) primary key not null,
+lib_version varchar2(64));
 
-CREATE TABLE Film
-(id_film NUMBER(4) PRIMARY KEY NOT NULL,
-titre VARCHAR2(64),
-tarif NUMBER(4),
-date_deb DATE,
-date_fin DATE,
-duree NUMBER(3),
-id_version NUMBER(4) REFERENCES Version(id_version) on delete set null,
-id_genre NUMBER(4) REFERENCES Genre(id_genre) on delete set null,
-id_restriction NUMBER(4) REFERENCES Restriction(id_restriction) on delete set null);
+create table Film
+(id_film number(4) primary key not null,
+titre varchar2(64),
+tarif number(4),
+date_deb date,
+date_fin date,
+duree number(3),
+id_version number(4) references Version(id_version) on delete set null,
+id_genre number(4) references Genre(id_genre) on delete set null,
+id_restriction number(4) references Restriction(id_restriction) on delete set null);
 
-CREATE TABLE Seance
-(date_seance DATE PRIMARY KEY NOT NULL,
-id_film NUMBER(4) REFERENCES Film(id_film) on delete cascade not null,
-id_salle NUMBER(3) REFERENCES Salle(id_salle) on delete cascade not null);
+create table Seance
+(date_seance date primary key not null,
+id_film number(4) references Film(id_film) on delete cascade not null,
+id_salle number(3) references Salle(id_salle) on delete cascade not null);
 
-CREATE TABLE Achat
-(id_achat NUMBER(8) PRIMARY KEY NOT NULL,
-date_achat DATE NOT NULL,
-id_film NUMBER(4) REFERENCES Film(id_film) on delete cascade not null,
-date_seance DATE REFERENCES Seance(date_seance) on delete cascade not null,
-type_tarif NUMBER(4) REFERENCES Tarif(type_tarif) on delete cascade not NULL);
+create table Achat
+(id_achat number(8) primary key not null,
+date_achat date not null,
+id_salle number(4) references Salle(id_salle) on delete cascade not null,
+date_seance date references Seance(date_seance) on delete cascade not null,
+type_tarif number(4) references Tarif(type_tarif) on delete cascade not NULL);
 
 prompt Base de données en place.
 
-SET ECHO ON
+set echo on
