49cee48a731642caa2e1c163a5285378e4b85f60
BerliozLeChat <neoflash93@gmail.com>
2015-03-19 16:53:45 +0000

    modif trigger, fix timestamp

--- trigger.sql
+++ trigger.sql
@@ -1,7 +1,7 @@
 -- Triggers de la base de données --
 
 -- Vérification du nombre de places restantes avant la réservation --
-
+show errors;
 create or replace trigger before_insert_achat
 before insert on Achat for each row
 declare
@@ -10,14 +10,16 @@ declare
 	
 begin
 	select count(*) into places_occupes from Achat
-	where id_seance = new.id_seance and id_salle = new.id_salle;
-	select capacite into max_place from Achat natural join Salle
-	where new.id_salle = id_salle;
+	where :old.id_seance = :new.id_seance;
+  
+  select capacite into max_place from Salle where id_salle = (select id_salle  from  Seance
+  where :old.id_seance=:new.id_seance);
 	
 	if places_occupes >= max_places then
 		raise reserv_error;
-	end if
+	end if;
 	
 	exception when reserv_error then
-	raise_application_error(-20001, "il n'y a plus de places disponibles pour cette séance");
-end
+	raise_application_error(-20001, "room is full);
+end;
+
